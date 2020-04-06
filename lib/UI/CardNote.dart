e42893decfde59568d76c0d70fd57b5db5b1ce6e
André Felipe <academicworks.adf@gmail.com>
2020-04-06 11:23:37 -0300

    Atualizando icone

--- lib/UI/CardNote.dart
+++ lib/UI/CardNote.dart
@@ -23,71 +23,73 @@ class _CardNoteState extends State<CardNote> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: 280,
-        width: 225,
-        child: Card(
-            elevation: 7.0,
-            color: Theme.of(context).primaryColor,
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: MediaQuery.of(context).size.height <= 600 ? 80 : 110,
-                  color: Colors.white60,
-                  child: Center(
-                    child: Image.asset('assets/note.png', color: Colors.black26),
+      child: SingleChildScrollView(
+              child: Container(
+          height: MediaQuery.of(context).size.height * 0.420,
+          width: MediaQuery.of(context).size.width * 0.600,
+          child: Card(
+              elevation: 7.0,
+              color: Theme.of(context).primaryColor,
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height <= 600 ? 80 : 110,
+                    color: Colors.white60,
+                    child: Center(
+                      child: Image.asset('assets/note.png', color: Colors.black26),
+                    ),
                   ),
-                ),
-                SizedBox(
-                    height: MediaQuery.of(context).size.height <= 400 ? 20 : 15),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: _newNote,
-                    cursorColor: Colors.white,
-                    decoration: InputDecoration(
-                        enabledBorder:
-                            UnderlineInputBorder(borderSide: BorderSide.none),
-                        focusedBorder:
-                            UnderlineInputBorder(borderSide: BorderSide.none),
-                        disabledBorder:
-                            UnderlineInputBorder(borderSide: BorderSide.none)),
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height <= 400 ? 20 : 15),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _newNote,
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                          enabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          focusedBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          disabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none)),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28),
+                    ),
                   ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      FlatButton(
-                        child: Icon(Icons.cancel, color: Colors.white, size: 30),
-                        onPressed: widget.setIsInserting,
-                      ),
-                      FlatButton(
-                        child: Icon(Icons.done, color: Colors.white, size: 30),
-                        onPressed: () {
-                          print(_newNote.text);
+                  Container(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Icon(Icons.cancel, color: Colors.white, size: 30),
+                          onPressed: widget.setIsInserting,
+                        ),
+                        FlatButton(
+                          child: Icon(Icons.done, color: Colors.white, size: 30),
+                          onPressed: () {
+                            print(_newNote.text);
 
-                          final data =
-                              CalcNote(cardColor: 'green', tema: _newNote.text);
-                          db.insert(data, 'CalcNotes').then((added) {
-                            widget.setIsInserting();
-                            widget.getNotes();
-                            _newNote.text = 'Tema';
-                          });
-                        },
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )),
+                            final data =
+                                CalcNote(cardColor: 'green', tema: _newNote.text);
+                            db.insert(data, 'CalcNotes').then((added) {
+                              widget.setIsInserting();
+                              widget.getNotes();
+                              _newNote.text = 'Tema';
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
